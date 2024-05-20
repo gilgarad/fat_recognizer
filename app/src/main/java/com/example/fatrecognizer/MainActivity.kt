0eb6a7832170ef2506f6bd155d74c3876913df69
isaacsim <emeprorsim@gmail.com>
2024-05-20 21:30:31 +0900

    git first commit

---
+++ app/src/main/java/com/example/fatrecognizer/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.fatrecognizer
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+//import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.parcelize.Parcelize
+
+class MainActivity : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        checkPermission()
+
+        main_btn_camera_open.setOnClickListener {
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                    takePictureIntent.resolveActivity(packageManager)?.also {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                    }
+                }
+        }
+    }
+
+    private fun checkPermission() {
+        var permission = mutableMapOf<String, String>()
+        permission["camera"] = Manifest.permission.CAMERA
+//        permission["storageRead"] = Manifest.permission.READ_EXTERNAL_STORAGE
+//        permission["storageWrite"] =  Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+        var denied = permission.count { ContextCompat.checkSelfPermission(this, it.value)  == PackageManager.PERMISSION_DENIED }
+
+        if(denied > 0 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            requestPermissions(permission.values.toTypedArray(), REQUEST_IMAGE_CAPTURE)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_IMAGE_CAPTURE) {
+            var count = grantResults.count { it == PackageManager.PERMISSION_DENIED }
+
+            if(count != 0) {
+                Toast.makeText(applicationContext, "권한을 동의해주세요.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+
+            main_img_photo.setImageBitmap(imageBitmap)
+        }
+    }
+}
